c4c35a32271e88574ecdb1aa7a7a390a1420f6be
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-02 10:17:24 +0530

    solution

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  // newStr = ""
+  // for (let i = 0; i < str.length; i++) {
+  //   newStr += str[str.length -i - 1];
+  // } 
+  // return newStr;
 
+  return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
