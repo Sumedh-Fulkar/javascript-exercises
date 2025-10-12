615511cd7d7f7cfef24730ae8bb62c3430f1bb4c
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-12 11:56:07 +0530

    palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  strArr = str
+          .toLowerCase()
+          .split("")
+          .filter((ch) => (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'));
+          
+  revStr = strArr.slice().reverse();
 
+  return revStr.join("") === strArr.join("");
 };
 
 // Do not edit below this line
