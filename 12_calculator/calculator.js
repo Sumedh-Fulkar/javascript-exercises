af993c648b7598d46f739571ed8f2a82a0832dc4
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-12 11:40:48 +0530

    object exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, cur) => total + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, cur) => total * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) {
+    return 1;
+  }
+  fact = 1;
+  for (let i = 2; i <= n; i++ ) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
