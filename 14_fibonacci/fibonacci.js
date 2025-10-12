caea61249bc4d9af5eff16dd76cb0c10a1dbb09d
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-12 12:07:10 +0530

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
 
+  if (typeof(n) === "string") {
+    n = Number(n);
+  }
+  if (n === 1 || n === 2) {
+    return 1;
+  }
+  let t1 = 1;
+  let t2 = 1;
+  let fib = 0;
+  for (let i = 0; i < n-2; i++) {
+    fib = t1 + t2;
+    t2 = t1;
+    t1 = fib;
+  }
+  return fib;
 };
 
 // Do not edit below this line
