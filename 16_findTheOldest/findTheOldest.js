9af6e289c7f35c84ee8ef9bb674602ee523bc6ab
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-12 12:31:43 +0530

    object assignment complete

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // Get the current year just once
+  const currentYear = new Date().getFullYear();
 
-};
+  return people.reduce((oldest, person) => {
+    const currentAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+
+    if (currentAge > oldestAge) {
+      return person;
+    } else {
+      return oldest;
+    }
+  });
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
